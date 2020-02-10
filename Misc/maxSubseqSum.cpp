3506c2829e6bb91e395355f759f12dda049aad56
vuzway9132 <13806680065@163.com>
2020-02-10 19:57:28 +0800

    -

---
+++ Misc/maxSubseqSum.cpp
@@ -0,0 +1,32 @@
+int MaxSubseqSum3(int A[], int N) {
+	return DivideAndConquer(A, 0, N-1);
+}
+int MaxSubseqSum4(int A[], int N) {
+	int MaxSum = 0, ThisSum = 0;
+	for (int i = 0; i < N; i++) {
+		ThisSum += A[i];
+		if (ThisSum > MaxSum) MaxSum = ThisSum;
+		if (ThisSum < 0) ThisSum = 0;
+	}
+	return MaxSum;
+}
+int DivideAndConquer(int A[], int left, int right) {
+	if (left == right) {
+		if (A[left] > 0) return A[left];
+		else return 0;
+	}
+	int centre = (left + right) / 2;
+	int MaxLeftSum = DivideAndConquer(A, left, centre);
+	int MaxRightSum = DivideAndConquer(A, centre+1, right);
+	int MaxLeftBorderSum = 0, LeftBorderSum = 0;
+	for (int i = centre; i >= left; i--) {
+		LeftBorderSum += A[i];
+		if (LeftBorderSum > MaxLeftBorderSum) MaxLeftBorderSum = LeftBorderSum;
+	}
+	MaxRightBorderSum = 0, RightBorderSum = 0;
+	for (int i = centre+1; i <= right; i++) {
+		RightBorderSum += A[i];
+		if (RightBorderSum > MaxRightBorderSum) MaxRightBorderSum = RightBorderSum;
+	}
+	return Max3(MaxLeftSum, MaxRightSum, MaxLeftBorderSum+MaxRightBorderSum);
+}
