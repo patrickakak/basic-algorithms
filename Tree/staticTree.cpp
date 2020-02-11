69575b187f87be58b80d188cabf1b505b06c8b14
vuzway9132 <13806680065@163.com>
2020-02-11 23:11:50 +0800

    -

--- Tree/staticTree.cpp
+++ Tree/staticTree.cpp
@@ -1,36 +1,23 @@
 /**
  * Static implementation of normal tree
  */
-#include <cstdio>
-#include <queue>
-#include <vector>
-using namespace std;
-
 #define maxn 100010
 struct Node {
 	int data, level;
 	vector<int> child;
 } node[maxn];
 int index = 0;
-
-int newNode(int v)
-{
+int newNode(int v) {
 	node[index].data = v;
 	node[index].child.clear();
 	return index++;
 }
-
-// Preorder or tree resembles the process of DFS
-void PreOrder(int root)
-{
+void PreOrder(int root) {
 	printf("%d ", node[root].data);
 	for (int i = 0; i < (int)node[root].child.size(); i++)
 		PreOrder(node[root].child[i]);
 }
-
-// Levelorder of tree resembles the process of BFS
-void LevelOrder(int root)
-{
+void LevelOrder(int root) {
 	queue<int> Q;
 	Q.push(root);
 	node[root].level = 0;
@@ -45,4 +32,3 @@ void LevelOrder(int root)
 		}
 	}
 }
-
