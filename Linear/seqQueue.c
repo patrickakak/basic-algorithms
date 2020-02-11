e0d5abf4f029516fdcaf92d1c63571de1cf97393
vuzway9132 <13806680065@163.com>
2020-02-11 16:19:52 +0800

    -

--- Linear/seqQueue.c
+++ Linear/seqQueue.c
@@ -1,32 +1,21 @@
-/* Implimentaion of queue(FIFO) in array:
- * Note: Front points to the front position of fist element, 
- *       Rear points to the last element 
- */
-/* defile ERROR, ElementType here */
 typedef int Position;
 struct QNode {
-	ElementType *Data; 	/* Array to store elements */
-	Position Front, Rear; 	/* Pointers to point to the front and rear */
-	int MaxSize; 	/* Max Capacity of queue */
+	ElementType *Data;
+	Position Front, Rear;
+	int MaxSize;
 };
 typedef struct QNode *Queue;
-
-Queue CreateQueue(int MaxSize)
-{
+Queue CreateQueue(int MaxSize) {
 	Queue Q = (Queue) malloc(sizeof(struct QNode));
 	Q->Data = (ElementType *) malloc(MaxSize * sizeof(ElementType));
 	Q->Front = Q->Rear = 0;
 	Q->MaxSize = MaxSize;
 	return Q;
 }
-
-bool IsFull(Queue Q)
-{
+bool IsFull(Queue Q) {
 	return ((Q->Rear+1)%Q->MaxSize == Q->Front);
 }
-
-bool Enqueue(Queue Q, ElementType X)
-{
+bool Enqueue(Queue Q, ElementType X) {
 	if (IsFull(Q)) {
 		printf("Full queue");
 		return false;
@@ -36,14 +25,10 @@ bool Enqueue(Queue Q, ElementType X)
 		return true;
 	}
 }
-
-bool IsEmpty(Queue Q)
-{
+bool IsEmpty(Queue Q) {
 	return (Q->Front == Q->Rear);
 }
-
-ElementType Dequeue(Queue Q)
-{
+ElementType Dequeue(Queue Q) {
 	if (IsEmpty(Q)) { 
 		printf("Empty queue");
 		return ERROR;
@@ -52,4 +37,3 @@ ElementType Dequeue(Queue Q)
 		return Q->Data[Q->Front];
 	}
 }
-
