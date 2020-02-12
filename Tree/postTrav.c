c8f66b4003581296948fe86dabfbc134dd43517a
vuzway9132 <13806680065@163.com>
2020-02-12 12:56:00 +0800

    -

--- Tree/postTrav.c
+++ Tree/postTrav.c
@@ -5,27 +5,21 @@ typedef struct TreeNode *BinTree;
 typedef BinTree Position;
 struct TreeNode {
 	ElementType Data;
-	BinTree Left;
-	BinTree Right;
-	int Nv; 	/* Number of visitation */
+	BinTree Left, Right;
+	int Nv;
 };
-
-/* Postorder traversal (recursive) */
-void PostOrderTraversal(BinTree BT)
-{
+// recursive
+void PostOrderTraversal(BinTree BT) {
 	if (BT) {
 		PostOrderTraversal(BT->Left);
 		PostOrderTraversal(BT->Right);
 		printf("%d", BT->Data);
 	}
 }
-
-/* Non-recursive postorder traversals */
-void PostOrderTraversal(BinTree BT)
-{
+// iterative
+void PostOrderTraversal(BinTree BT) {
 	BinTree T = BT;
 	Stack S = CreatStack(MaxSize);
-
 	while (T || !IsEmpty(S)) {
 		while (T) {
 			T->Nv = 1;
@@ -35,7 +29,7 @@ void PostOrderTraversal(BinTree BT)
 		if (!IsEmpty(S)) {
 			T = Pop(S); T->Nv++;
 			if (!T->Right)
-				T->Nv++; 	/* If the right subtree is NULL */
+				T->Nv++;
 			if (T->Nv == 3) {
 				printf("%5d", T->Data);
 				T = NULL;
@@ -46,4 +40,3 @@ void PostOrderTraversal(BinTree BT)
 		}
 	}
 }
-
