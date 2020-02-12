cd64f9e4dc5690e5cef87b2613479ba58db437dc
vuzway9132 <13806680065@163.com>
2020-02-12 13:00:11 +0800

    -

---
+++ Tree/levelTrav.c
@@ -0,0 +1,16 @@
+/**
+ * Level order traversal
+ */
+void LevelOrderTraversal(BinTree BT)
+{
+	queue<BinTree> Q;
+	BinTree T;
+	if (!BT) return;
+	Enqueue(Q, BT);
+	while (!IsEmptyQ(Q)) {
+		T = Dequeue(Q);
+		printf("%d", T->Data);
+		if (T->Left) Enqueue(Q, T->Left);
+		if (T->Right) Enqueue(Q, T->Right);
+	}
+}
