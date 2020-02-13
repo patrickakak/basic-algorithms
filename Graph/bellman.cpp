c8e6f6ed5f824da2754450d98f2d14c6865e45cb
vuzway9132 <13806680065@163.com>
2020-02-13 10:09:25 +0800

    -

--- Graph/bellman.cpp
+++ Graph/bellman.cpp
@@ -2,18 +2,11 @@
  * Bellman-Ford algorithm to handle negative-cycle
  * T = O(VE)
  */
-
-struct Node {
-	int v, dis;
-};
-
+struct Node { int v, dis; };
 vector<Node> Adj[MAXV];
-int n;
-int d[MAXV];
-
+int n, d[MAXV];
 // Return false if there's a negative-cycle
-bool Bellman_Ford(int s)
-{
+bool Bellman_Ford(int s) {
 	fill(d, d+MAXV, INF);
 	d[s] = 0;
 	for (int i = 0; i < n-1; i++)
@@ -21,16 +14,13 @@ bool Bellman_Ford(int s)
 			for (int j = 0; j < (int)Adj[u].size(); j++) {
 				int v = Adj[u][j].v;
 				int dis = Adj[u][j].dis;
-				if (d[u] + dis < d[v])
-					d[v] = d[u] + dis;
+				if (d[u] + dis < d[v]) d[v] = d[u] + dis;
 			}
 	for (int u = 0; u < n; u++)
 		for (int j = 0; j < (int)Adj[u].size(); j++) {
 			int v = Adj[u][j].v;
 			int dis = Adj[u][j].dis;
-			if (d[u] + dis < d[v])
-				return false;
+			if (d[u] + dis < d[v]) return false;
 		}
 	return true;
 }
-
