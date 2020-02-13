017389ff07f9c026735c278c3ccc3c5d501d8b85
vuzway9132 <13806680065@163.com>
2020-02-13 10:36:38 +0800

    -

--- Misc/shiftLR.c
+++ Misc/shiftLR.c
@@ -3,7 +3,6 @@
  * Left or Right Shift in a confined array space.
  **************************************************
  */
-
 /**
  * Chirality:
  *
@@ -12,39 +11,27 @@
  * parts as two huge elements, both of them have only mirrored once, so
  * the order of those two is reversed for sure.
  */
-void Swap(ElementType *X, ElementType *Y)
-{
+void Swap(ElementType *X, ElementType *Y) {
 	ElementType tmp;
-	
 	tmp = *X; *X = *Y; *Y = tmp;
 }
-
-void Mirror(ElementType S[], int L, int R)
-{
-	int i, j;
-
-	for (i=L, j=R; i < j; i++, j--)
+void Mirror(ElementType S[], int L, int R) {
+	for (int i=L, j=R; i < j; i++, j--)
 		Swap(&S[i], &S[j]);
 }
-
 /* M: offset (M<N)
  * Divide N elements into two parts: 0~M-1 and M~N-1
  */
-void LeftShift(ElementType S[], int N, int M)
-{
+void LeftShift(ElementType S[], int N, int M) {
 	if (M >= N) return;
 	Mirror(S, 0, M-1);
 	Mirror(S, M, N-1);
 	Mirror(S, 0, N-1);
 }
-
 /* M: offset (M<N)
  * Divide N elements into two parts: 0~N-M-1 and N-M~N-1.
  */
-void RightShift(ElementType S[], int N, int M)
-{
+void RightShift(ElementType S[], int N, int M) {
 	if (M >= N) return;
-	
 	LeftShift(S, N, N-M);
 }
-
