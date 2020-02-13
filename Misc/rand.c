2e71544dc3bbb92b8432f445a45fddb08e01e214
vuzway9132 <13806680065@163.com>
2020-02-13 10:43:37 +0800

    -

--- Misc/rand.c
+++ Misc/rand.c
@@ -5,18 +5,11 @@
 #include <math.h>
 #include <stdlib.h>
 #include <time.h>
-
-int main()
-{
-	int i;
-
+int main() {
 	srand((unsigned) time(NULL));
-    
     // Generate number in interval [10000, 60000]
-	for (i = 0; i < 10; i++)
+	for (int i = 0; i < 10; i++)
 		printf("%d ", (int)(round(1.0*rand()/RAND_MAX*50000 + 10000)));
 	putchar('\n');
-
 	return 0;
 }
-
