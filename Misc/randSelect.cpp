a75ae1c2dbf005b686beb9cab164ec3f4e997756
vuzway9132 <13806680065@163.com>
2020-02-13 10:44:34 +0800

    -

--- Misc/randSelect.cpp
+++ Misc/randSelect.cpp
@@ -7,12 +7,9 @@
 #include <cmath>
 #include <algorithm>
 using namespace std;
-
 const int maxn = 100010;
 int A[maxn], n;
-
-int randPartition(int A[], int L, int R)
-{
+int randPartition(int A[], int L, int R) {
 	int p = (round(1.0*rand()/RAND_MAX*(R-L) + L));
 	swap(A[p], A[L]);
 	int tmp = A[L];
@@ -25,26 +22,16 @@ int randPartition(int A[], int L, int R)
 	A[L] = tmp;
 	return L;
 }
-
-int randSelect(int A[], int L, int R, int Kth)
-{
-	if (L == R)
-		return A[L];
+int randSelect(int A[], int L, int R, int Kth) {
+	if (L == R) return A[L];
 	int p = randPartition(A, L, R);
 	int M = p - L + 1;
-	if (Kth == M)
-		return A[p];
-	else if (Kth < M)
-		return randSelect(A, L, p-1, Kth);
-	else
-		return randSelect(A, p+1, R, Kth-M);
+	if (Kth == M) return A[p];
+	else if (Kth < M) return randSelect(A, L, p-1, Kth);
+	else return randSelect(A, p+1, R, Kth-M);
 }
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+int main() {
 	srand((unsigned) time(NULL));
-
 	int sum = 0, sum1 = 0;
 	scanf("%d", &n);
 	for (int i = 0; i < n; i ++) {
@@ -52,10 +39,7 @@ int main()
 		sum += A[i];
 	}
 	randSelect(A, 0, n-1, n/2);
-	for (int i = 0; i < n/2; i++)
-		sum1 += A[i];
+	for (int i = 0; i < n/2; i++) sum1 += A[i];
 	printf("%d\n", (sum-sum1)-sum1);
-
 	return 0;
 }
-
