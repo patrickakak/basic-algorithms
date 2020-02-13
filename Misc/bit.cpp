994642d04397eba69ed59e1fb3a247c64506803f
vuzway9132 <13806680065@163.com>
2020-02-13 10:34:17 +0800

    -

--- Misc/bit.cpp
+++ Misc/bit.cpp
@@ -4,34 +4,22 @@
  * (the most classic use of BIT is to calculate # of elements 
  * less than the current element)
  */
-#include <cstdio>
+#include <iostream>
 #include <cstring>
-
+using namespace std;
 const int maxn = 100010;
 #define lowbit(i) ((i)&(-i))
 int c[maxn];
-
-void update(int x, int v)
-{
-	for (int i = x; i < maxn; i += lowbit(i))
-		c[i] += v;
+void update(int x, int v) {
+	for (int i = x; i < maxn; i += lowbit(i)) c[i] += v;
 }
-
-int getSum(int x)
-{
+int getSum(int x) {
 	int sum = 0;
-	for (int i = x; i > 0; i -= lowbit(i))
-		sum += c[i];
+	for (int i = x; i > 0; i -= lowbit(i)) sum += c[i];
 	return sum;
 }
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	int n, x;
-
-	/**
-	 *  _________________________________________
+int main() {
+	/*  _________________________________________
 	 * |                                1000 C[8]| lowbit(8)=8
 	 * |_________________________________________|
 	 *  _________________                    |
@@ -47,6 +35,7 @@ int main()
 	 * |  1    2    3    4    5    6    7    8   |
 	 * -------------------------------------------
 	 */
+	int n, x;
 	scanf("%d", &n);
 	memset(c, 0, sizeof(c));
 	for (int i = 0; i < n; i++) {
@@ -54,8 +43,5 @@ int main()
 		update(x, 1);
 		printf("%d\n", getSum(x-1));
 	}
-
 	return 0;
 }
-
-
