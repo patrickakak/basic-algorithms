8d97e55691d5a610610aaffae0fcc28add949863
vuzway9132 <13806680065@163.com>
2020-02-13 10:04:10 +0800

    -

--- Dynamic_Programming/lis.cpp
+++ Dynamic_Programming/lis.cpp
@@ -2,24 +2,18 @@
  * Longest Increasing Sequence, can be discontinuous
  * (T = O(N^2) by using dynamic programming)
  */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
 using namespace std;
-
 const int N = 100;
 // dp[i] is the LIS with the i-th element as the end
 int A[N], dp[N];
-
-int main()
-{
+int main() {
 	// 8
 	// 1 2 3 -9 3 9 0 11
 	int n;
-
 	scanf("%d", &n);
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &A[i]);
-	
+	for (int i = 1; i <= n; i++) scanf("%d", &A[i]);
 	// dp[i] = max{1, dp[j]+1}
 	// (j = 1, 2, ..., i-1 && A[j] < A[i])
 	int ans = -1;
@@ -34,5 +28,3 @@ int main()
 	printf("%d\n", ans);
 	return 0;
 }
-
-
