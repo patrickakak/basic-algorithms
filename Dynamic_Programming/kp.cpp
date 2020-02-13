566c5a715500b1c9ed00e7dd8d96e18f380011a0
vuzway9132 <13806680065@163.com>
2020-02-13 10:01:55 +0800

    -

--- Dynamic_Programming/kp.cpp
+++ Dynamic_Programming/kp.cpp
@@ -1,30 +1,21 @@
 /**
  * Knapsack problem (NP problem)
  */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
 using namespace std;
-
 const int maxn = 100;
 const int maxv = 1000;
 int w[maxn], c[maxn], dp[maxv];
-
-
-int main()
-{
-	int n, V;
-
+int main() {
 	// 5 8
 	// 3 5 1 2 2
 	// 4 5 2 1 3
+	int n, V;
 	scanf("%d%d", &n, &V);
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &w[i]);
-	for (int i = 1; i <= n; i++)
-		scanf("%d", &c[i]);
-	for (int v = 0; v <= V; v++)
-		dp[v] = 0;
-	
+	for (int i = 1; i <= n; i++) scanf("%d", &w[i]);
+	for (int i = 1; i <= n; i++) scanf("%d", &c[i]);
+	for (int v = 0; v <= V; v++) dp[v] = 0;
 	// dp[v] = max{ dp[v], dp[v-w[i]] + c[i] }
 	//          (1<= i <=n, w[i] <= v <= V)
 	for (int i = 1; i <= n; i++)
@@ -32,12 +23,8 @@ int main()
 			dp[v] = max(dp[v], dp[v-w[i]] + c[i]);
 	int max = 0;
 	for (int v = 0; v <= V; v++)
-		if (dp[v] > max)
-			max = dp[v];
+		if (dp[v] > max) max = dp[v];
 	// 10
 	printf("%d\n", max);
-
 	return 0;
 }
-
-
