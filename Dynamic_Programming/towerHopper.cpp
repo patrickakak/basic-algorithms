928265667619ec4aabca8485df1b3b88521488fb
vuzway9132 <13806680065@163.com>
2020-02-13 10:00:41 +0800

    -

--- Dynamic_Programming/towerHopper.cpp
+++ Dynamic_Programming/towerHopper.cpp
@@ -1,20 +1,13 @@
 /**
  * Tower Hopper problem
  */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
 using namespace std;
-
 const int maxn = 1000;
 int f[maxn][maxn], dp[maxn][maxn];
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
-	int n;
-
-	/**
-	 *           5           dp[1][1]
+int main() {
+	/*           5           dp[1][1]
 	 *          / \            .
 	 *         8   3           .
 	 *        / \ / \          .
@@ -24,19 +17,16 @@ int main()
 	 *    /  \ / \/ \ / \      .
 	 *   9    5  3   9   4   dp[n][1] ~ dp[n][n]
 	 */
+	int n;
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++)
 		for (int j = 1; j <= i; j++)
 			scanf("%d", &f[i][j]);
-
 	for (int j = 1; j <= n; j++)
 		dp[n][j] = f[n][j];
 	for (int i = n-1; i >= 1; i--)
 		for (int j = 1; j <= i; j++)
 			dp[i][j] = max(dp[i+1][j], dp[i+1][j+1]) + f[i][j];
-
 	printf("%d\n", dp[1][1]);
 	return 0;
 }
-
-
