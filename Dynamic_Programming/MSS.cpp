a694101886eb9d2cb8ffe6d622cbab690cd3f538
vuzway9132 <13806680065@163.com>
2020-02-13 10:06:21 +0800

    -

--- Dynamic_Programming/MSS.cpp
+++ Dynamic_Programming/MSS.cpp
@@ -2,38 +2,28 @@
  * Maximum Subsequence Sum
  * (T = O(N) by using dynamic programming)
  */
-#include <cstdio>
+#include <iostream>
 #include <algorithm>
 using namespace std;
-
 const int maxn = 10010;
 // dp[i] is the MSS with the i-th element as the end
 int A[maxn], dp[maxn];
-
-int main()
-{
+int main() {
 	// 6
 	// -2 11 -4 13 -5 -2
-	int n, k;
-	
+	int n;
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-		scanf("%d", &A[i]);
-
+	for (int i = 0; i < n; i++) scanf("%d", &A[i]);
 	// the state transfer equation must be without aftereffect
 	// dp[i] = max{ A[i], dp[i-1]+A[i] }
 	dp[0] = A[0];
 	for (int i = 1; i < n; i++)
 		dp[i] = max(A[i], dp[i-1]+A[i]);
-
-	k = 0;
+	int k = 0;
 	for (int i = 1; i < n; i++)
 		if (dp[i] > dp[k])
 			k = i;
 	// {-2, 11, -4, 13, -5, -2}
 	printf("%d\n", dp[k]);
-
 	return 0;
 }
-
-
