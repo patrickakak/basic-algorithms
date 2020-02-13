45f60342c39922ad0eeabecb8c3b6aae9c3e8fb3
vuzway9132 <13806680065@163.com>
2020-02-13 10:35:03 +0800

    -

--- Misc/gcd.c
+++ Misc/gcd.c
@@ -2,20 +2,13 @@
  * Greatest Common Divisor
  * (Euclidean algorithm)
  */
-#include <cstdio>
-
-int gcd(int a, int b)
-{
+#include <stdio.h>
+int gcd(int a, int b) {
 	return !b ? a : gcd(b, a % b);
 }
-
-int main()
-{
+int main() {
 	int m, n;
-
 	while (scanf("%d%d", &m, &n) != EOF)
 		printf("%d\n", gcd(m, n));
-
 	return 0;
 }
-
