9078db94d89cfb4d7928497bc18eff88f94e5e7b
vuzway9132 <13806680065@163.com>
2020-02-13 10:32:48 +0800

    -

--- Integer_Factorization/primeFactors.cpp
+++ Integer_Factorization/primeFactors.cpp
@@ -1,48 +1,33 @@
 /* 
  * Integer factorization (into primes)
  */
-#include <cstdio>
+#include <iostream>
 #include <cmath>
-
+using namespace std;
 const int maxn = 100010;
 int prime[maxn], pNum = 0;
 struct factor {
 	int x, cnt;
 } fac[10];	// 10 is enough for 32bit integer
-
-bool is_prime(int n)
-{
-	if (n == 1)
-		return false;
+bool is_prime(int n) {
+	if (n == 1) return false;
 	int sqr = (int)sqrt(1.0 * n);
 	for (int i = 2; i <= sqr; i++)
-		if (n % i == 0)
-			return false;
+		if (n % i == 0) return false;
 	return true;
 }
-
-void Find_Prime()
-{
+void Find_Prime() {
 	for (int i = 1; i < maxn; i++)
-		if (is_prime(i))
-			prime[pNum++] = i;
+		if (is_prime(i)) prime[pNum++] = i;
 }
-
-int main()
-{
-	// freopen("tst.txt", "r", stdin);
+int main() {
 	Find_Prime();
-
 	int n, num = 0;
 	scanf("%d", &n);
-	if (n == 1)
-		printf("1=1");
+	if (n == 1) printf("1=1");
 	else {
 		printf("%d=", n);
 		int sqr = (int)sqrt(1.0 * n);
-		// If there's a prime factor of n, then:
-		//   1: all prime factors of n are smaller than sqrt(n) or...
-		//   2: there's only one prime larger than sqrt(n)
 		for (int i = 0; i < pNum && prime[i] <= sqr; i++) {
 			if (n % prime[i] == 0) {
 				fac[num].x = prime[i];
@@ -69,5 +54,3 @@ int main()
 	}
 	return 0;
 }
-
-
