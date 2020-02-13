747f04ce9e179819bed71dfd9f45d824297043eb
vuzway9132 <13806680065@163.com>
2020-02-13 10:35:32 +0800

    -

--- Misc/lcm.c
+++ Misc/lcm.c
@@ -2,25 +2,16 @@
  * Least Common Multiple
  */
 #include <cstdio>
-
-int gcd(int a, int b)
-{
+int gcd(int a, int b) {
 	return !b ? a : gcd(b, a % b);
 }
-
-int lcm(int a, int b)
-{
+int lcm(int a, int b) {
 	return a / gcd(a, b) * b;
 }
-
-int main()
-{
+int main() {
 	int m, n, r;
-
 	scanf("%d%d", &m, &n);
 	r = lcm(m, n);
 	printf("%d\n", r);
-
 	return 0;
 }
-
