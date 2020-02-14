da0705709c941e9b667773a54abd7d4a2e7cd382
vuzway9132 <13806680065@163.com>
2020-02-14 22:08:39 +0800

    -

--- Sort/quickSort.cpp
+++ Sort/quickSort.cpp
@@ -1,12 +1,9 @@
-
 #include <cstdio>
 #include <algorithm>
 #include <iostream>
 using namespace std;
-
 const int maxn = 10000;
-int Partition(int A[], int left, int right)
-{
+int Partition(int A[], int left, int right) {
 	int tmp = A[left];
 	while (left < right) {
 		while (left < right && A[right] > tmp) right--;
@@ -17,23 +14,17 @@ int Partition(int A[], int left, int right)
 	A[left] = tmp;
 	return left;
 }
-
-void quickSort(int A[], int left, int right)
-{
+void quickSort(int A[], int left, int right) {
 	if (left < right) {
 		int pos = Partition(A, left, right);
 		quickSort(A, left, pos-1);
 		quickSort(A, pos+1, right);
 	}
 }
-
-void QSort(int A[], int n)
-{
+void QSort(int A[], int n) {
 	quickSort(A, 0, n-1);
 }
-
-int main()
-{
+int main() {
 	int A[maxn] = {8,6,12,94,0,4,5};
 	QSort(A, 7);
 	for (int i = 0; i < 7; i++) {
@@ -41,4 +32,3 @@ int main()
 	}
 	return 0;
 }
-
