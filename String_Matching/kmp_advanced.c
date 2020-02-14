ad3efa76496262eaaf098436ebde75ad98f48285
vuzway9132 <13806680065@163.com>
2020-02-14 22:25:34 +0800

    -

--- String_Matching/kmp_advanced.c
+++ String_Matching/kmp_advanced.c
@@ -4,51 +4,35 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
 typedef int Position;
 #define NotFound -1
-
 void BuildMatch(char *pattern, int *match);
 Position KMP(char *string, char *pattern);
-
-int main()
-{
+int main() {
 	char string[] = "This is a simple example.";
 	char pattern[] = "simple";
-
 	Position p = KMP(string, pattern);
-	if (p == NotFound)
-		printf("Not Found.\n");
-	else
-		printf("%s\n", p+string);
+	if (p == NotFound) printf("Not Found.\n");
+	else printf("%s\n", p+string);
 	return 0;
 }
-
 /* Improve next[] to be nextVal[] */
-void BuildNextVal(char *pattern, int *nextVal, int *next)
-{
+void BuildNextVal(char *pattern, int *nextVal, int *next) {
 	Position i, j;
 	int m = strlen(pattern);
-
 	nextVal[0] = 0;
 	for (i = 1; i < m; i++) {
 		j = next[i-1];
 		while (j>0 && pattern[j]==pattern[i])
 			j = next[j-1];
-		if (j == 0)
-			nextVal[i] = 0;
-		else if (j == next[i-1])
-			nextVal[i] = next[i];
-		else
-			nextVal[i] = j;
+		if (j == 0) nextVal[i] = 0;
+		else if (j == next[i-1]) nextVal[i] = next[i];
+		else nextVal[i] = j;
 	}
 }
-
-void BuildMatch(char *pattern, int *match)
-{
+void BuildMatch(char *pattern, int *match) {
 	Position i, j;
 	int m = strlen(pattern);
-
 	match[0] = 0;
 	for (i=1, j=0; i < m; i++)
 		if (pattern[i] == pattern[j]) {
@@ -60,20 +44,15 @@ void BuildMatch(char *pattern, int *match)
 			if (pattern[j] == pattern[i]) {
 				match[i] = j+1;
 				j++;
-			} else
-				match[i] = 0;
+			} else match[i] = 0;
 		}
 }
-
-Position KMP(char *string, char *pattern)
-{
+Position KMP(char *string, char *pattern) {
 	int n = strlen(string);
 	int m = strlen(pattern);
 	Position s, p, *match, *nextVal;
-
 	if (n < m)
 		return NotFound;
-
 	match = (Position *) malloc(sizeof(Position) * m);
 	BuildMatch(pattern, match);
 	nextVal = (Position *) malloc(sizeof(Position) * m);
@@ -83,12 +62,9 @@ Position KMP(char *string, char *pattern)
 		if (string[s] == pattern[p]) {
 			s++;
 			p++;
-		} else if (p > 0)
-			p = nextVal[p];
-		else
-			s++;
+		} else if (p > 0) p = nextVal[p];
+		else s++;
 	free(match);
 	free(nextVal);
 	return (p == m) ? (s-m) : NotFound;
 }
-
