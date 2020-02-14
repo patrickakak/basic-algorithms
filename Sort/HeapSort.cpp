c43dfd6b1be7eac7d891a189886da9800dcb550a
vuzway9132 <13806680065@163.com>
2020-02-14 21:50:26 +0800

    -

--- Sort/HeapSort.cpp
+++ Sort/HeapSort.cpp
@@ -7,34 +7,24 @@
  * Note: but in fact, it's not that useful, Sedgewick sequence 
  * Shell sort normally is better.
  */
-
-/* Re-organize N elements in array A[] to be a MaxHeap(A[p] as root) */
-void PercDown(ElementType A[], int p, int N)
-{
+void PercDown(ElementType A[], int p, int N) {
 	int Parent, Child;
 	ElementType X;
-
-	X = A[p];	/* Fetch the value from root */
+	X = A[p];
 	for (Parent = p; (Parent*2+1) < N; Parent = Child) {
 		Child = 2*Parent + 1;
-		if ((Child != N-1) && (A[Child] < A[Child+1]))
-			Child++;	/* Child points to the larger one */
-		if (X >= A[Child]) break;	/* Spot the right place */
-		else A[Parent] = A[Child]; 	/* Percolate X down */
+		if ((Child != N-1) && (A[Child] < A[Child+1])) Child++;
+		if (X >= A[Child]) break;
+		else A[Parent] = A[Child];
 	}
 	A[Parent] = X;
 }
-
-void HeapSort(ElementType A[], int N) 
-{
+void HeapSort(ElementType A[], int N)  {
 	int i;
-
-	for (i = N/2-1; i >= 0; i--)	/* Build MaxHeap to avoid extra space */
+	for (i = N/2-1; i >= 0; i--)
 		PercDown(A, i, N);
-	
 	for (i = N-1; i > 0; i--) {
-		Swap(&A[0], &A[i]);		/* Delete the top element from MaxHeap */
+		Swap(&A[0], &A[i]);
 		PercDown(A, 0, i);
 	}
 }
-
