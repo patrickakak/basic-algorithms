888787635763cb4c13578b8d683d91b566d1f765
vuzway9132 <13806680065@163.com>
2020-02-14 22:15:13 +0800

    -

--- Hashing/separateChaining.c
+++ Hashing/separateChaining.c
@@ -4,11 +4,9 @@
  * p (i.e. Expected value) = 
  *                           \ 1 + α/2 (ASLs)
  */
-#define KEYLENGTH 15	/* The maximum length of the keyword string */
+#define KEYLENGTH 15
 typedef char ElementType[KEYLENGTH+1];
-typedef int Index;	/* The address type of hash */
-
-/******** Definition of single list ********/
+typedef int Index;
 typedef struct LNode *PtrToLNode;
 struct LNode {
 	ElementType Data;
@@ -16,55 +14,40 @@ struct LNode {
 };
 typedef PtrToLNode Position;
 typedef PtrToLNode List;
-/******** Definition of single list ********/
-
 typedef struct TblNode *HashTable;
 struct TblNode {
 	int TableSize;
 	List Heads;
 };
-
-HashTable CreateTable(int TableSize)
-{
+HashTable CreateTable(int TableSize) {
 	HashTable H;
 	int i;
-
 	H = (HashTable) malloc(sizeof(struct TblNode));
 	H->TableSize = NextPrime(TableSize);
-
-	/* List head array */
 	H->Heads = (List) malloc(H->TableSize * sizeof(struct LNode));
-	for (i = 0; i < H->TableSize; i++) {	/* Initialize the head nodes */
+	for (i = 0; i < H->TableSize; i++) {
 		H->Heads[i].Data[0] = '\0';
 		H->Heads[i].Next = NULL;
 	}
 	return H;
 }
-
-Position Find(HashTable H, ElementType Key)
-{
+Position Find(HashTable H, ElementType Key) {
 	Position P;
 	Index Pos;
-
-	Pos = Hash(Key, H->TableSize);	/* The initial hash position */
-	P = H->Heads[Pos].Next;		/* Start from the first node of current list */
+	Pos = Hash(Key, H->TableSize);
+	P = H->Heads[Pos].Next;
 	while (P && strcmp(P->Data, Key))
-		P = P->Next;	/* Have not reached the end and find the Key yet */ 
-
-	return P; 	/* P points to the spotted node or NULL */
+		P = P->Next;
+	return P;
 }
-
-bool Insert(HashTable H, ElementType Key)
-{
+bool Insert(HashTable H, ElementType Key) {
 	Position P, NewCell;
 	Index Pos;
-
 	P = Find(H, Key);
-	if (!P) { 	/* Couldn't find the Key, insert it */
+	if (!P) {
 		NewCell = (Position) malloc(sizeof(struct LNode));
 		strcpy(NewCell->Data, Key);
-		Pos = Hash(Key, H->TableSize); 	/* Initial hash position */
-		/* Insert NewCell as the first node of H->Heads[Pos] list */
+		Pos = Hash(Key, H->TableSize);
 		NewCell->Next = H->Heads[Pos].Next;
 		H->Heads[Pos].Next = NewCell; 
 		return true;
@@ -73,13 +56,10 @@ bool Insert(HashTable H, ElementType Key)
 		return false;
 	}
 }
-
-void DestroyTable(HashTable H)
-{
+void DestroyTable(HashTable H) {
 	int i;
 	Position P, Tmp;
-
-	for (i = 0; i < H->TableSize; i++) {	/* Free every node of every list */
+	for (i = 0; i < H->TableSize; i++) {
 		P = H->Heads[i].Next;
 		while (P) {
 			Tmp = P->Next;
@@ -87,7 +67,6 @@ void DestroyTable(HashTable H)
 			P = Tmp;
 		}
 	}
-	free(H->Heads);		/* Free the array of head nodes */
-	free(H); 	/* Free hash node */
+	free(H->Heads);
+	free(H);
 }
-
