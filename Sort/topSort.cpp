b95f947b32ed1dff0a459a5898287cca69192350
vuzway9132 <13806680065@163.com>
2020-02-14 21:59:18 +0800

    -

--- Sort/topSort.cpp
+++ Sort/topSort.cpp
@@ -3,39 +3,30 @@
  * TopSort algorithm for AOV(i.e. Activity On Vertex) of directed graph.
  * (could be used to dectect DAG(i.e. directed acyclic graph))
  */
-
 /* TopSort graph, indexes of vertices will be stored into TopOrader[] array 
  * (Note: Prepare a rather large queue space, it can get tricky)
  */
-bool TopSort(LGraph Graph, Vertex TopOrder[])
-{
+bool TopSort(LGraph Graph, Vertex TopOrder[]) {
 	int Indegree[MaxVertexNum], cnt;
 	Vertex V;
 	PtrToAdjVNode W;
 	Queue Q = CreateQueue(MaxVertexNum);
-
-	for (V = 0; V < Graph->Nv; V++)		/* Initialize Indegree[] */
+	for (V = 0; V < Graph->Nv; V++)
 		Indegree[V] = 0;
-
-	for (V = 0; V < Graph->Nv; V++)		/* Traverse graph to get Indegree[] */
+	for (V = 0; V < Graph->Nv; V++)
 		for (W = Graph->G[V].FirstEdge; W; W = W->Next)
 			Indegree[W->AdjV]++;
-
-	for (V = 0; V < Graph->Nv; V++)		/* Enqueue vertices with 0 indegree */
+	for (V = 0; V < Graph->Nv; V++)
 		if (Indegree[V] == 0)
 			Enqueue(Q, V);
-
-	cnt = 0;	/* TopSort begins */
+	cnt = 0;
 	while (!IsEmpty(Q)) {
-		V = Dequeue(Q);		/* Dequeue a vertex with 0 indegree */
-		TopOrder[cnt++] = V;	/* Store it in TopOrder[] array */
-		/* For each vertices next to V */
+		V = Dequeue(Q);
+		TopOrder[cnt++] = V;
 		for (W = Graph->G[V].FirstEdge; W; W = W->Next)
 			if (--Indegree[W->AdjV] == 0)
 				Enqueue(Q, W->AdjV);
 	}
-
-	if (cnt != Graph->Nv) return false;		/* There's a cycle in graph */
+	if (cnt != Graph->Nv) return false;
 	else return true;
 }
-
