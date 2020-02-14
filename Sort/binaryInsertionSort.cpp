a2566fb179e03af835ee3813337025166d59f9f6
vuzway9132 <13806680065@163.com>
2020-02-14 21:48:03 +0800

    -

--- Sort/binaryInsertionSort.cpp
+++ Sort/binaryInsertionSort.cpp
@@ -1,28 +1,19 @@
 /**
  * Binary insertion sort: T=O(n^2)
- *
  * improve insertion sort by finding the position to insert more effeciently
  */
-
-int findPlace(ElementType A[], ElementType tmp, int high)
-{
+int findPlace(ElementType A[], ElementType tmp, int high) {
 	int mid, low = 0;
-
 	while (low <= high) {
 		mid = (low + high)/2;
-		if (tmp >= A[mid])	/* Add '=' in order to move less elements */
-			low = mid + 1;
-		else
-			high = mid - 1;
+		if (tmp >= A[mid]) low = mid + 1;
+		else high = mid - 1;
 	}
 	return high+1;
 }
-
-void BinaryInsertionSort(ElementType A[], int N)
-{
+void BinaryInsertionSort(ElementType A[], int N) {
 	int i, j, high, p;
 	ElementType tmp;
-
 	for (high=0, i=1; i < N; i++) {
 		tmp = A[i];
 		p = findPlace(A, tmp, high);
@@ -32,4 +23,3 @@ void BinaryInsertionSort(ElementType A[], int N)
 		high += 1;
 	}
 }
-
