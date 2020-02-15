ec914917276c84f7e191ab5c3229536198093a02
vuzway9132 <13806680065@163.com>
2020-02-15 14:00:45 +0800

    -

--- sort/bubbleSort.cpp
+++ sort/bubbleSort.cpp
@@ -4,10 +4,9 @@
  * extra S = O(1)
  */
 void BubbleSort(ElementType A[], int N) {
-	int P, i, flag;
-	for (P = N-1; P >= 0; P--) {
-		flag = 0;
-		for (i = 0; i < P; i++)
+	for (int P = N-1; P >= 0; P--) {
+		int flag = 0;
+		for (int i = 0; i < P; i++)
 			if (A[i] > A[i+1]) {
 				Swap(A[i], A[i+1]);
 				flag = 1;
