a9711fd0049d4946c9e4bc6a19047e2964009dd2
vuzway9132 <13806680065@163.com>
2020-08-01 22:09:33 +0800

    -

---
+++ search/random_select.py
@@ -0,0 +1,11 @@
+def RANDOMIZED_SELECT(A, p, r, i):
+	if p == r:
+		return A[p]
+	q = RANDOMIZED_PARTITION(A, p, r)
+	k = q - p + 1
+	if i == k:
+		return A[q]
+	elif i < k:
+		return RANDOMIZED_SELECT(A, p, q-1, i)
+	else:
+		RANDOMIZED_SELECT(A, q+1, r, i-k)
