189b6e0264c4cc478ea63f0d4bd36773f1619862
chopchap <13806680065@163.com>
2020-12-29 15:08:14 +0800

    -

---
+++ bfs_template.py
@@ -0,0 +1,17 @@
+def bfs(graph, start, end):
+
+  queue = []
+  queue.append([start])
+  visited.add(start)
+  
+  while queue:
+    node = queue.pop()
+    visited.add(node)
+    
+    process(node)
+    nodes = generate_related_nodes(node)
+    queue.push(nodes)
+    
+  # other processing work
+  ...
+  
