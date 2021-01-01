d3fd28680ba061987a467b9456886eed0ea14c69
chopchap <13806680065@163.com>
2021-01-01 15:03:32 +0800

    -

---
+++ union_find_template.java
@@ -0,0 +1,30 @@
+public class QuickUnionUF {
+  private int[] roots;
+  
+  public QuickUnionUF(int N) {
+    roots = new int[N];
+    for (int i = 0; i < N; i++) {
+      roots[i] = i;
+    }
+  }
+  
+  private int findRoot(int i) {
+    int root = i;
+    while (root != roots[root])
+      root = roots[root]
+     while (i != roots[i]) {
+       int tmp = roots[i]; roots[i] = root; i = tmp;
+     }
+     return root;
+  }
+  
+  public boolean connected(int p, int q) {
+    return findRoot(p) == findRoot(q);
+  }
+  
+  public void union(int p, int q) {
+    int qroot = findRoot(q);
+    int proot = findRoot(p);
+    roots[proot] = qroot;
+  }
+}
