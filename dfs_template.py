0681b60a1b3e3c3ed827cade8cf8febe709a7691
chopchap <13806680065@163.com>
2021-01-01 19:31:45 +0800

    -

--- dfs_template.py
+++ dfs_template.py
@@ -1,8 +1,9 @@
 visited = set()
 def dfs(node, visited):
-  visited.add(node)
-  # process current node here.
-  ...
-  for next_node in node.children():
-    if not next_node in visited:
-      dfs(next_node, visited)
+    visited.add(node)
+    # process current node here.
+    ...
+    for next_node in node.children():
+        if not next_node in visited:
+            dfs(next_node, visited)
+            
