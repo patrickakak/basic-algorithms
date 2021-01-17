e7033c6191b747de66a30542dec4179f4dbd85a6
chopchap <13806680065@163.com>
2021-01-17 21:37:15 +0800

    -

--- dfs_template.py
+++ dfs_template.py
@@ -7,3 +7,20 @@ def dfs(node, visited):
         if not next_node in visited:
             dfs(next_node, visited)
             
+
+def dfs(self, tree):
+    if tree.root is None:
+        return []
+    
+    visited, stack = [], [tree.root]
+    
+    while stack:
+        node = stack.pop()
+        visited.add(node)
+        
+        process(ndoe)
+        nodes = generate_related_nodes(node)
+        stack.push(nodes)
+        
+    # other processing work
+    ...
