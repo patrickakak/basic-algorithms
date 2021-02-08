9f4e21f347589e23ad3f884c9015d64a66747cda
chopchap <13806680065@163.com>
2021-02-08 11:27:58 +0800

    -

--- Binary Search Template III.java
+++ Binary Search Template III.java
@@ -1,23 +1,23 @@
 int binarySearch(int[] nums, int target) {
-    if (nums == null || nums.length == 0)
-        return -1;
+	if (nums == null || nums.length == 0)
+		return -1;
 
-    int left = 0, right = nums.length - 1;
-    while (left + 1 < right){
-        // Prevent (left + right) overflow
-        int mid = left + (right - left) / 2;
-        if (nums[mid] == target) {
-            return mid;
-        } else if (nums[mid] < target) {
-            left = mid;
-        } else {
-            right = mid;
-        }
-    }
+	int left = 0, right = nums.length - 1;
+	while (left + 1 < right){
+		// Prevent (left + right) overflow
+		int mid = left + (right - left) / 2;
+		if (nums[mid] == target) {
+			return mid;
+		} else if (nums[mid] < target) {
+			left = mid;
+		} else {
+			right = mid;
+		}
+	}
 
-    // Post-processing:
-    // End Condition: left + 1 == right
-    if (nums[left] == target) return left;
-    if (nums[right] == target) return right;
-    return -1;
+	// Post-processing:
+	// End Condition: left + 1 == right
+	if (nums[left] == target) return left;
+	if (nums[right] == target) return right;
+	return -1;
 }
