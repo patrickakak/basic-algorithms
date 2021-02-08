5b9f2c13bd6d34b6b955610ddd3f20291f7416a4
chopchap <13806680065@163.com>
2021-02-08 14:32:37 +0800

    -

--- Binary Search Template I.py
+++ Binary Search Template I.py
@@ -2,17 +2,17 @@
 # ---------------------------------------------------------------------------------------
 # 	1. Most basic and elementary form of Binary Search
 # 	2. Search Condition can be determined without comparing 
-# 	to the element's neighbors (or use specific elements around it)
+# 	   to the element's neighbors (or use specific elements around it)
 # 	3. No post-processing required because at each step, you are checking to see if the 
-# 	element has been found. If you reach the end, then you know the element is not found
+# 	   element has been found. If you reach the end, then you know the element is not found
 
 
 # Distinguishing Syntax:
 # ---------------------------------------------------------------------------------------
-# Initial Condition: left = 0, right = length - 1
-# Termination: left > right
-# Searching Left: right = mid - 1
-# Searching Right: left = mid + 1
+# 	1. Initial Condition: left = 0, right = length - 1
+# 	2. Termination: left > right
+# 	3. Searching Left: right = mid - 1
+# 	4. Searching Right: left = mid + 1
 def binarySearch(nums, target):
     """
     :type nums: List[int]
