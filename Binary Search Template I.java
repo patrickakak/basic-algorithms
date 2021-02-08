8a047bbad6ffdecfb87fef44eab8dc34247e4f14
chopchap <13806680065@163.com>
2021-02-08 11:23:15 +0800

    -

--- Binary Search Template I.java
+++ Binary Search Template I.java
@@ -1,13 +1,13 @@
-int binarySearch(int[] nums, int target){
-  if(nums == null || nums.length == 0)
+int binarySearch(int[] nums, int target) {
+  if (nums == null || nums.length == 0)
     return -1;
 
   int left = 0, right = nums.length - 1;
-  while(left <= right){
+  while (left <= right) {
     // Prevent (left + right) overflow
     int mid = left + (right - left) / 2;
-    if(nums[mid] == target){ return mid; }
-    else if(nums[mid] < target) { left = mid + 1; }
+    if (nums[mid] == target) { return mid; }
+    else if (nums[mid] < target) { left = mid + 1; }
     else { right = mid - 1; }
   }
 
