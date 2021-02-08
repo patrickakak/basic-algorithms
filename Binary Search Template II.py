231ad049af2738fced869c32b2be995713dcd815
chopchap <13806680065@163.com>
2021-02-08 14:30:45 +0800

    -

--- Binary Search Template II.py
+++ Binary Search Template II.py
@@ -1,3 +1,20 @@
+# Key Attributes:
+# ---------------------------------------------------------------------------------
+# 	1. An advanced way to implement Binary Search.
+# 	2. Search Condition needs to access element's immediate right neighbor
+# 	3. Use element's right neighbor to determine if condition is met and 
+# 	   decide whether to go left or right
+# 	4. Gurantees Search Space is at least 2 in size at each step
+# 	5. Post-processing required. Loop/Recursion ends when you have 1 element left.
+# 	   Need to assess if the remaining element meets the condition.
+	
+	
+# Distinguishing Syntax:
+# ---------------------------------------------------------------------------------
+# 	1. Initial Condition: left = 0, right = length
+# 	2. Termination: left == right
+# 	3. Searching Left: right = mid
+# 	4. Searching Right: left = mid + 1
 def binarySearch(nums, target):
     """
     :type nums: List[int]
