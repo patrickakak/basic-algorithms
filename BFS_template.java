26150fdb704e48c387265db7b2241582e8339d86
chopchap <13806680065@163.com>
2021-03-14 09:52:49 +0800

    -

--- BFS_template.java
+++ BFS_template.java
@@ -3,25 +3,25 @@
  * Return the length of the shortest path between root and target node.
  */
 int BFS(Node root, Node target) {
-    Queue<Node> queue;  // store all nodes which are waiting to be processed
-    int step = 0;       // number of steps neeeded from root to current node
-    // initialize
-    add root to queue;
-    // BFS
-    while (queue is not empty) {
-        step = step + 1;
-        // iterate the nodes which are already in the queue
-        int size = queue.size();
-        for (int i = 0; i < size; ++i) {
-            Node cur = the first node in queue;
-            return step if cur is target;
-            for (Node next : the neighbors of cur) {
-                add next to queue;
-            }
-            remove the first node from queue;
-        }
+  Queue<Node> queue;  // store all nodes which are waiting to be processed
+  int step = 0;       // number of steps neeeded from root to current node
+  // initialize
+  add root to queue;
+  // BFS
+  while (queue is not empty) {
+    step = step + 1;
+    // iterate the nodes which are already in the queue
+    int size = queue.size();
+    for (int i = 0; i < size; ++i) {
+      Node cur = the first node in queue;
+      return step if cur is target;
+      for (Node next : the neighbors of cur) {
+        add next to queue;
+      }
+      remove the first node from queue;
     }
-    return -1;          // there is no path from root to target
+  }
+  return -1;          // there is no path from root to target
 }
 
 
@@ -30,30 +30,30 @@ int BFS(Node root, Node target) {
  * Return the length of the shortest path between root and target node.
  */
 int BFS(Node root, Node target) {
-    Queue<Node> queue;  // store all nodes which are waiting to be processed
-    Set<Node> visited;  // store all the nodes that we've placed in Queue
-    int step = 0;       // number of steps neeeded from root to current node
-    // initialize
-    add root to queue;
-    add root to visited;
-    // BFS
-    while (queue is not empty) {
-        step = step + 1;
-        // iterate the nodes which are already in the queue
-        int size = queue.size();
-        for (int i = 0; i < size; ++i) {
-            Node cur = the first node in queue;
-            return step if cur is target;
-            for (Node next : the neighbors of cur) {
-                if (next is not in used) {
-                    add next to queue;
-                    add next to visited;
-                }
-            }
-            remove the first node from queue;
+  Queue<Node> queue;  // store all nodes which are waiting to be processed
+  Set<Node> visited;  // store all the nodes that we've placed in Queue
+  int step = 0;       // number of steps neeeded from root to current node
+  // initialize
+  add root to queue;
+  add root to visited;
+  // BFS
+  while (queue is not empty) {
+    step = step + 1;
+    // iterate the nodes which are already in the queue
+    int size = queue.size();
+    for (int i = 0; i < size; ++i) {
+      Node cur = the first node in queue;
+      return step if cur is target;
+      for (Node next : the neighbors of cur) {
+        if (next is not in used) {
+          add next to queue;
+          add next to visited;
         }
+      }
+      remove the first node from queue;
     }
-    return -1;          // there is no path from root to target
+  }
+  return -1;          // there is no path from root to target
 }
 
 
