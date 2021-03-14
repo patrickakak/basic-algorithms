37045d426af9911043caedad781f593897084750
chopchap <13806680065@163.com>
2021-03-14 09:52:14 +0800

    -

--- DFS_template.java
+++ DFS_template.java
@@ -2,14 +2,14 @@
  * Return true if there is a path from cur to target.
  */
 boolean DFS(Node cur, Node target, Set<Node> visited) {
-    return true if cur is target;
-    for (next : each neighbor of cur) {
-        if (next is not in visited) {
-            add next to visted;
-            return true if DFS(next, target, visited) == true;
-        }
+  return true if cur is target;
+  for (next : each neighbor of cur) {
+    if (next is not in visited) {
+      add next to visted;
+      return true if DFS(next, target, visited) == true;
     }
-    return false;
+  }
+  return false;
 }
 
 
@@ -17,21 +17,21 @@ boolean DFS(Node cur, Node target, Set<Node> visited) {
  * Return true if there is a path from cur to target.
  */
 boolean DFS(int root, int target) {
-    Set<Node> visited;
-    Stack<Node> stack;
-    add root to stack;
-    while (s is not empty) {
-        Node cur = the top element in stack;
-        remove the cur from the stack;
-        return true if cur is target;
-        for (Node next : the neighbors of cur) {
-            if (next is not in visited) {
-                add next to visited;
-                add next to stack;
-            }
-        }
+  Set<Node> visited;
+  Stack<Node> stack;
+  add root to stack;
+  while (s is not empty) {
+    Node cur = the top element in stack;
+    remove the cur from the stack;
+    return true if cur is target;
+    for (Node next : the neighbors of cur) {
+      if (next is not in visited) {
+        add next to visited;
+        add next to stack;
+      }
     }
-    return false;
+  }
+  return false;
 }
 
 
