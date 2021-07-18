e9bfea404426bdef1c5c1b5a0d0eb5dc30d68c93
chopchap <22060468@zju.edu.cn>
2021-07-18 22:57:43 +0800

    -

--- sort/insertionSort.cpp
+++ sort/insertionSort.cpp
@@ -5,6 +5,9 @@
  * Actually: T(N,I) = O(N+I) (I for inversion(s)),
  * average number of inversions in N different elements: N*(N-1)/4, 
  * so average time complexity: T=Ω(N^2)
+ *
+ * An array is partially-sorted if # of inversions <= c*N
+ * For partially-sorted arrays, Insertion sort runs in linear time (i.e. O(N))
  */
 void InsertionSort(ElementType A[], int N) {
 	for (int P = 1; P < N; P++) {
